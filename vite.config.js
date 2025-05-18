382f7bbca3666f55c5f792620fa3de78c53f8924
Token Revolution s.r.l.s. <94378706+TokenRevolution@users.noreply.github.com>
2025-05-18 16:36:53 +0200

    back

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   server: {
@@ -9,22 +8,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        admin: resolve(__dirname, 'admin.html'),
-        'admin-login': resolve(__dirname, 'admin-login.html'),
-        playlist: resolve(__dirname, 'playlist.html'),
-        dm65: resolve(__dirname, 'dm65.html'),
-        dm66: resolve(__dirname, 'dm66.html'),
-        dm19: resolve(__dirname, 'dm19.html'),
-        'pnrr-labs': resolve(__dirname, 'pnrr-labs.html'),
-        'pnrr-classroom': resolve(__dirname, 'pnrr-classroom.html'),
-        sostituzioni: resolve(__dirname, 'sostituzioni.html'),
-        'calendario-impegni': resolve(__dirname, 'calendario-impegni.html'),
-        organigramma: resolve(__dirname, 'organigramma.html')
-      }
-    }
+    emptyOutDir: true
   }
 }); 
